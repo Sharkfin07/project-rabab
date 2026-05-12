ebe02ebf12250954b28818357bd7e476dea38af0
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-05-12 08:58:06 +0700

    fix: Registered fonts the right way

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 	return (
 		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<main>
-				<h1 className="font-bold">Kota Dua Waktu</h1>
+				<h1 className="font-minangkabau text-4xl">Kota Dua Waktu</h1>
 			</main>
 		</div>
 	);
